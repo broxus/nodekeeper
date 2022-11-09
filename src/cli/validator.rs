e28a4b8b7ff93bef561b3a8a53d50c62addba771
Ivan Kalinin <i.kalinin@dexpa.io>
2022-11-09 04:24:59 +0100

    Check elector state during election

--- src/cli/validator.rs
+++ src/cli/validator.rs
@@ -1,9 +1,10 @@
+use std::collections::BTreeMap;
 use std::sync::Arc;
 use std::time::Duration;
 
 use anyhow::{Context, Result};
 use argh::FromArgs;
-use ton_executor::blockchain_config;
+use nekoton_abi::{KnownParamType, KnownParamTypePlain, MaybeRef, UnpackAbi, UnpackAbiPlain};
 
 use crate::node_tcp_rpc::{ConfigWithId, NodeStats, NodeTcpRpc, RunningStats};
 use crate::node_udp_rpc::NodeUdpRpc;
@@ -19,9 +20,13 @@ pub struct Cmd {
     #[argh(option, default = "120")]
     max_time_diff: u16,
 
-    /// elections offset (in seconds). 600 seconds default
+    /// elections start offset (in seconds). 600 seconds default
     #[argh(option, default = "600")]
-    elections_offset: u32,
+    elections_start_offset: u32,
+
+    /// elections end offset (in seconds). 120 seconds default
+    #[argh(option, default = "120")]
+    elections_end_offset: u32,
 
     /// min retry interval (in seconds). 10 seconds default
     #[argh(option, default = "10")]
@@ -41,7 +46,8 @@ impl Cmd {
         let mut manager = ValidationManager {
             ctx,
             max_time_diff: std::cmp::max(self.max_time_diff as i32, 5),
-            elections_offset: self.elections_offset,
+            elections_start_offset: self.elections_start_offset,
+            elections_end_offset: self.elections_end_offset,
         };
 
         self.min_retry_interval = std::cmp::max(self.min_retry_interval, 1);
@@ -68,7 +74,8 @@ impl Cmd {
 struct ValidationManager {
     ctx: CliContext,
     max_time_diff: i32,
-    elections_offset: u32,
+    elections_start_offset: u32,
+    elections_end_offset: u32,
 }
 
 impl ValidationManager {
@@ -96,6 +103,9 @@ impl ValidationManager {
                 config: blockchain_config,
             } = node_tcp_rpc.get_config_all().await?;
 
+            let elector_address = blockchain_config
+                .elector_address()
+                .context("invalid elector address")?;
             let timings = blockchain_config
                 .elector_params()
                 .context("invalid elector params")?;
@@ -123,24 +133,56 @@ impl ValidationManager {
             let timeline = Timeline::compute(&timings, &current_vset, target_block_info.gen_utime);
             tracing::info!("timeline: {timeline}");
 
-            match timeline {
+            let until_elections_end = match timeline {
                 Timeline::BeforeElections {
                     until_elections_start,
                 } => {
-                    interval = until_elections_start + self.elections_offset;
+                    tracing::info!("waiting for the elections to start");
+                    interval = until_elections_start + self.elections_start_offset;
+                    continue;
                 }
                 Timeline::Elections {
                     since_elections_start,
                     until_elections_end,
                 } => {
-                    // TODO: elect
-
-                    interval = until_elections_end;
+                    if let Some(offset) = self
+                        .elections_start_offset
+                        .checked_sub(since_elections_start)
+                    {
+                        tracing::info!("too early to participate in elections");
+                        interval = offset;
+                        continue;
+                    } else if let Some(offset) =
+                        self.elections_end_offset.checked_sub(until_elections_end)
+                    {
+                        tracing::info!("too late to participate in elections");
+                        interval = offset;
+                        continue;
+                    } else {
+                        until_elections_end
+                    }
                 }
                 Timeline::AfterElections { until_round_end } => {
+                    tracing::info!("waiting for the new round to start");
                     interval = until_round_end;
+                    continue;
                 }
-            }
+            };
+
+            // PARTICIATE
+            let elector_data = subscription.get_elector_data(elector_address).await?;
+            let election_id = match elector_data.current_election.0 {
+                Some(current_election) => current_election.elect_at,
+                None => {
+                    tracing::info!("no current elections in the elector state");
+                    continue;
+                }
+            };
+            tracing::info!("election id: {election_id}");
+
+            // TODO: elect
+
+            interval = until_elections_end;
         }
     }
 
@@ -165,6 +207,40 @@ impl ValidationManager {
             }
             tokio::time::sleep(interval).await;
         }
+    }
+}
+
+impl Subscription {
+    async fn get_elector_data(
+        &self,
+        elector_address: ton_types::UInt256,
+    ) -> Result<PartialElectorData> {
+        let elector = ton_block::MsgAddressInt::AddrStd(ton_block::MsgAddrStd {
+            workchain_id: -1,
+            address: elector_address.into(),
+            ..Default::default()
+        });
+
+        let elector_state = self
+            .get_account_state(&elector)
+            .await
+            .context("failed to get elector state")?
+            .context("elector not found")?;
+
+        let ton_block::AccountState::AccountActive { state_init } = elector_state.storage.state else {
+            anyhow::bail!("elector account is not active");
+        };
+        let data = state_init.data.context("elector data is empty")?;
+        let state: PartialElectorData = ton_abi::TokenValue::decode_params(
+            elector_data_params(),
+            data.into(),
+            &ton_abi::contract::ABI_VERSION_2_1,
+            true,
+        )
+        .context("failed to parse elector data")?
+        .unpack()?;
+
+        Ok(state)
     }
 }
 
@@ -230,4 +306,48 @@ impl Timeline {
             until_round_end: round_end.saturating_sub(now),
         }
     }
+}
+
+fn elector_data_params() -> &'static [ton_abi::Param] {
+    once!(Vec<ton_abi::Param>, || PartialElectorData::param_type())
+}
+
+#[derive(Debug, UnpackAbiPlain, KnownParamTypePlain)]
+struct PartialElectorData {
+    #[abi]
+    current_election: MaybeRef<CurrentElectionData>,
+    #[abi]
+    credits: BTreeMap<ton_types::UInt256, ton_block::Grams>,
+}
+
+#[derive(Debug, UnpackAbi, KnownParamType)]
+struct CurrentElectionData {
+    #[abi(uint32)]
+    elect_at: u32,
+    #[abi(uint32)]
+    elect_close: u32,
+    #[abi(gram)]
+    min_stake: u128,
+    #[abi(gram)]
+    total_stake: u128,
+    #[abi]
+    members: BTreeMap<ton_types::UInt256, ElectionMember>,
+    #[abi(bool)]
+    failed: bool,
+    #[abi(bool)]
+    finished: bool,
+}
+
+#[derive(Debug, UnpackAbi, KnownParamType)]
+struct ElectionMember {
+    #[abi(gram)]
+    msg_value: u64,
+    #[abi(uint32)]
+    created_at: u32,
+    #[abi(uint32)]
+    max_factor: u32,
+    #[abi(uint256)]
+    src_addr: ton_types::UInt256,
+    #[abi(uint256)]
+    adnl_addr: ton_types::UInt256,
 }
