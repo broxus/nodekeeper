51f00b2a8eabd9e7dd463d23641e8c01744e83a3
Ivan Kalinin <i.kalinin@dexpa.io>
2023-01-27 15:06:29 +0100

    Add `--version/-v` flag

--- src/main.rs
+++ src/main.rs
@@ -48,5 +48,43 @@ async fn main() -> Result<()> {
         tracing_subscriber::fmt::fmt().without_time().init();
     }
 
-    argh::from_env::<cli::App>().run().await
+    argh::from_env::<ArgsOrVersion<cli::App>>().0.run().await
+}
+
+struct ArgsOrVersion<T: argh::FromArgs>(T);
+
+impl<T: argh::FromArgs> argh::TopLevelCommand for ArgsOrVersion<T> {}
+
+impl<T: argh::FromArgs> argh::FromArgs for ArgsOrVersion<T> {
+    fn from_args(command_name: &[&str], args: &[&str]) -> Result<Self, argh::EarlyExit> {
+        /// Also use argh for catching `--version`-only invocations
+        #[derive(argh::FromArgs)]
+        struct Version {
+            /// print version information and exit
+            #[argh(switch, short = 'v')]
+            pub version: bool,
+        }
+
+        match Version::from_args(command_name, args) {
+            Ok(v) if v.version => Err(argh::EarlyExit {
+                output: format!(
+                    "{} {}",
+                    command_name.first().unwrap_or(&""),
+                    env!("CARGO_PKG_VERSION")
+                ),
+                status: Ok(()),
+            }),
+            Err(exit) if exit.status.is_ok() => {
+                let help = match T::from_args(command_name, &["--help"]) {
+                    Ok(_) => unreachable!(),
+                    Err(exit) => exit.output,
+                };
+                Err(argh::EarlyExit {
+                    output: format!("{help}  -v, --version     print version information and exit"),
+                    status: Ok(()),
+                })
+            }
+            _ => T::from_args(command_name, args).map(Self),
+        }
+    }
 }
