746b42ea5c0fb439743f29f77ebf70d2b38a6437
Ivan Kalinin <i.kalinin@dexpa.io>
2023-03-16 21:47:08 +0100

    Check `cargo` before build

--- src/cli/init/node.rs
+++ src/cli/init/node.rs
@@ -835,6 +835,15 @@ async fn clone_repo<P: AsRef<Path>>(url: &Url, branch: &Option<String>, target: 
 async fn build_node<P: AsRef<Path>>(target: P, features: &[String]) -> Result<PathBuf> {
     let target = target.as_ref();
 
+    if let Err(e) = exec(&mut Command::new("cargo").arg("-V")).await {
+        print_error(format!(
+            "Failed to check `cargo` version. Rust is not installed properly.\n  \
+               Please use the following command to install:\n\n\
+            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh\n"
+        ));
+        return Err(e);
+    }
+
     let mut command = Command::new("cargo");
     command
         .current_dir(target)
